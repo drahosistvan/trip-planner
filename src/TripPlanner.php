84ace95c271db6f9d476350ca0c61e928bdef9e1
Istvan Drahos <drahos.istvan@gmail.com>
2023-08-10 22:42:49 +0200

    base route calculations

--- src/TripPlanner.php
+++ src/TripPlanner.php
@@ -4,12 +4,39 @@ namespace Isti\TripPlanner;
 
 class TripPlanner
 {
-    public function __construct(public array $locations)
+    public function __construct(private readonly array $locations)
     {
     }
 
     public function calculateOptimalRoute(): string
     {
-        return '';
+        return implode('', $this->topologicalSort());
+    }
+
+    private function topologicalSort(): array
+    {
+        $visited = [];
+        $stack = [];
+
+        foreach ($this->locations as $location => $dependsOn) {
+            if (!isset($visited[$location]) || !$visited[$location]) {
+                $this->topologicalSortUtil($location, $stack, $visited);
+            }
+        }
+
+        return $stack;
+    }
+
+    private function topologicalSortUtil($location, &$stack, &$visited): void
+    {
+        $visited[$location] = true;
+
+        if (isset($this->locations[$location])) {
+            if (!isset($visited[$this->locations[$location]]) || !$visited[$this->locations[$location]]) {
+                $this->topologicalSortUtil($this->locations[$location], $stack, $visited);
+            }
+        }
+
+        $stack[] = $location;
     }
 }
--- tests/Unit/TripPlannerTest.php
+++ tests/Unit/TripPlannerTest.php
@@ -1,17 +1,53 @@
 <?php
 
-describe('TripPlanner', function () {
+describe('TripPlanner input - output', function () {
     it('can be instantiated', function () {
         $tripPlanner = new \Isti\TripPlanner\TripPlanner([]);
         expect($tripPlanner)->toBeInstanceOf(\Isti\TripPlanner\TripPlanner::class);
     });
 
-    it('can be instantiated with locations', function () {
-        $locations = [
-            'x' => ['y'],
-            'y',
-        ];
-        $tripPlanner = new \Isti\TripPlanner\TripPlanner($locations);
-        expect($tripPlanner->locations)->toBe($locations);
+    it('has a method to calculate the optimal route', function () {
+        $tripPlanner = new \Isti\TripPlanner\TripPlanner([]);
+        expect($tripPlanner->calculateOptimalRoute())->toBeString();
     });
+});
+
+describe('TripPlanner route calculation', function () {
+    it('can calculate the optimal route for a simple trip', function (array $locations, string $expected) {
+        $tripPlanner = new \Isti\TripPlanner\TripPlanner($locations);
+        expect($tripPlanner->calculateOptimalRoute())->toBe($expected);
+    })->with([
+        [
+            [
+                'x' => '',
+            ],
+            'x',
+        ],
+        [
+            [
+                'x' => 'y',
+                'y' => '',
+            ],
+            'yx',
+        ],
+        [
+            [
+                'x' => '',
+                'y' => 'z',
+                'z' => '',
+            ],
+            'xzy',
+        ],
+        [
+            [
+                'u' => '',
+                'v' => 'w',
+                'w' => 'z',
+                'x' => 'u',
+                'y' => 'v',
+                'z' => '',
+            ],
+            'uzwvxy',
+        ]
+    ]);
 });
